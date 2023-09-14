7d771863b53f70e14b0dd28fd44cd9843a4c00ad
Tony <anthonyhenriquecba8@gmail.com>
2023-09-14 11:13:11 -0400

    Relação de ForeignKey e inclines com admin.TabularInline

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -4,16 +4,22 @@ from site_setup.models import SiteSetup
 from site_setup.models import MenuLink
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path'
-    list_display_links = 'id', 'text', 'url_or_path'
-    search_fields = 'id', 'text', 'url_or_path'
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path'
+#     list_display_links = 'id', 'text', 'url_or_path'
+#     search_fields = 'id', 'text', 'url_or_path'
+
+
+class MenuLinkInLine(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description'
+    inlines = MenuLinkInLine,
 
     def has_add_permission(self, request: HttpRequest) -> bool:
         return not SiteSetup.objects.exists()
