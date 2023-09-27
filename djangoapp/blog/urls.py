8ca8d5e494ce3beee1b5af636e7d37955f9a4989
Tony <anthonyhenriquecba8@gmail.com>
2023-09-26 23:03:30 -0400

    Usando DetailView com pPOst para criar PostDetailView + Usando a CBV DetialView com Post para Criar PostDetailView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from blog.views import (PostListView, post, page, CreatedByListView,
-                        CategoryListView, SearchListView, TagListView)
+from blog.views import (PostListView, PostDetailView, PageDetailView,
+                        CreatedByListView, CategoryListView, SearchListView,
+                        TagListView)
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='index'),
-    path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>', page, name='page'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post'),
+    path('page/<slug:slug>', PageDetailView.as_view(), name='page'),
     path('created_by/<int:author_pk>/',
          CreatedByListView.as_view(), name='created_by'),
     path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
